85ba0b633aef2a5bac6ef40071edbfdeb9e6429d
Kent Ketter <kent.ketter1@gmail.com>
2019-06-06 11:38:05 -0700

    tests passing

--- src/test/java/Quotes/GSONQuoteGrabberTest.java
+++ src/test/java/Quotes/GSONQuoteGrabberTest.java
@@ -2,12 +2,29 @@ package Quotes;
 
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.nio.file.NoSuchFileException;
+
+import static Quotes.GSONQuoteGrabber.quoteGetter;
 import static org.junit.Assert.*;
 
 public class GSONQuoteGrabberTest {
 
     @Test
-    public void quoteGetter() {
+    public void quoteGetterTest() {
+        String filePath = "/Users/rkktt/401/Quotes/assets/recentquotes.json";
+        String output1 = quoteGetter(filePath);
+        String output2 = quoteGetter(filePath);
+        String output3 = quoteGetter(filePath);
+        assertNotEquals(output1, output2);
+        assertNotEquals(output2, output3);
+        assertNotEquals(output1, output3);
+    }
+    @Test
+    public void fileNotFoundExceptionTest() {
+        String filePath = "CTRL+V/USERS";
+        assertEquals("File Not Found", quoteGetter(filePath));
 
     }
+
 }
