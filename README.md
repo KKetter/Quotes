5e80db373022a2b158299cbe52fe78339e3171e1
Kent Ketter <kent.ketter1@gmail.com>
2019-06-11 13:45:38 -0700

    starting ron swanson quote read in

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # Quotes
 
-This is a lab completed by [Kent](https://github.com/KKetter) and [Tim](https://github.com/GoldBeardSea).
+Initial lab completed by [Kent](https://github.com/KKetter) and [Tim](https://github.com/GoldBeardSea).
+Lab 09 only [Kent](https://github.com/KKetter)
 
 ## Contents
 
-It is a command line interface operated by scanner. Clone down the repository. Open in intellij and run the application.
+Program purpose - command line interface operated by scanner. 
+
+Setup
+* Clone down the repository. 
+* "gradle init" from command line in cloned directory.
+* Open in intellij and run the application.
+
 The execution takes in a filepath to your json file in order to grab the quotes. Preferrably you would use the included
 quote file in assets. You can read in any file that at least has an author and text structured like the Quote class
 constructor. 
+
+Lab 09
+* Make a request to an API to get a random quote
+* Ensure that if your app has errors in connecting to the Internet, you instead display a random quote from your file.
+* When a quote comes back from a request, it’s also cached in the json file
--- src/main/java/Quotes/GSONQuoteGrabber.java
+++ src/main/java/Quotes/GSONQuoteGrabber.java
@@ -5,22 +5,37 @@ import com.google.gson.Gson;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.BufferedReader;
 
 public class GSONQuoteGrabber {
 
     public static String quoteGetter(String filePath) {
         Quote randomQuote;
+        //GET to https://ron-swanson-quotes.herokuapp.com/v2/quotes
         try {
+            //Read from GSON file
+//            Gson gson = new Gson();
+//            File file = new File(filePath);
+//            FileReader reader = new FileReader(file);
+//
+//            Quote[] quotes = gson. fromJson(reader, Quote[].class);
+//
+//
+//            int randomIndex = (int)Math.floor(quotes.length * (Math.random()));
+//            randomQuote = quotes[randomIndex];
+//            return String.valueOf(randomQuote);
+            //starting API attempt
+            URL url = new URL("https://ron-swanson-quotes.herokuapp.com/v2/quotes");
+            //what is this?
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            //goes to the internet
+            BufferedReader reader = new BufferedReader(new InputStreamReader((con.getInputStream())));
+            //get an array with one quote back ["Ron Swanson Gem"]
+            //turn it back into GSON
             Gson gson = new Gson();
-            File file = new File(filePath);
-            FileReader reader = new FileReader(file);
-
-            Quote[] quotes = gson. fromJson(reader, Quote[].class);
+            Quote[] quotes = gson.fromJson(reader, Quote[].class);
 
 
-            int randomIndex = (int)Math.floor(quotes.length * (Math.random()));
-            randomQuote = quotes[randomIndex];
-            return String.valueOf(randomQuote);
         }
         catch (FileNotFoundException e){
             e.printStackTrace();
